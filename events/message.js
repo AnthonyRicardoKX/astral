d95ad13639f477c36888f0b40c95d770e4ded7fc
AnthonyRicardoKX <belzeharkxarus@ymail.com>
2021-03-09 12:32:14 +0700

    Update index automatically load modularized commands and events

---
+++ events/message.js
@@ -0,0 +1,19 @@
+const warned = [];
+
+module.exports = async (client, message) => {
+
+    // Ignores message if author is bot
+    if (message.author.bot) return;
+
+    // Ignores message if does not contain prefix
+    if (message.content.indexOf(process.env.APP_PREFIX) !== 0) return;
+
+    let userMessage = message.content.split(/ +/g);
+    let userCommand = userMessage.shift().slice(process.env.APP_PREFIX.length).toLowerCase();
+    let command = client.commands[userCommand] || client.commands[client.aliases[userCommand]];
+    
+    if (command !== undefined) {
+        console.log(userMessage);
+        command.run(client, message, userMessage);
+    }
+};
--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.commands = [];
+client.aliases = [];
+
+// Iteratively read ./commands/genshin directory to enable all command files
+fs.readdir('./commands/genshin/', (err, files) => {
+  if (err) console.error(err);
+  console.log(`Loading Genshin-related ${files.length} commands.`);
+  files.forEach(file => {
+    if (file.split(".").slice(-1)[0] !== "js") return;
+    let props = require(`./commands/genshin/${file}`);
+    client.commands[props.help.name] = props;
+    if (props.init) props.init(Client);
+    props.conf.aliases.forEach(alias => {
+      client.aliases[alias] = props.help.name;
+    });
+  });
+  console.log(client.commands);
+  console.log(client.aliases);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+// Iteratively read ./events directoy to setup all events listed as files
+fs.readdir('./events/', (err, files) => {
+  if (err) console.error(err);
+  console.log(`Loading ${files.length} events.`);
+  files.forEach(file => {
+    if (file.split(".").slice(-1)[0] !== "js") return
+    const eventName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
 });
 
 client.login(process.env.API_TOKEN);
